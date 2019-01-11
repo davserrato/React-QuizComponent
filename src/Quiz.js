1d0406e962de1a3b602c0bd6d36249a82560376f
David <david.serrato@santacruzcounty.us>
2019-01-10 16:16:04 -0800

    Fix for errors in module 3

--- src/Quiz.js
+++ src/Quiz.js
@@ -7,13 +7,13 @@ class Quiz extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {quiz_position: 0};
+        this.state = {quiz_position: 1};
     }
 
     render() {
         return (
             <div>
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position]}/>
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
             </div>
         )
     }
--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react'
 class QuizQuestionButton extends Component {
     render() {
         return(
-            <li><button>{this.props.quiz_question.answer_options[0]}</button></li>
+            <li><button>{this.props.button_text}</button></li>
         )
     }
 }
